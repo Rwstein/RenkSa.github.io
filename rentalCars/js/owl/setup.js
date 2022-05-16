45fb086579c9e890df21c9aa3fbc6a7e288a2549
Renk Willy da Mota Sá <82468145+RenkSa@users.noreply.github.com>
2022-05-16 16:20:29 -0300

    inserindo site

---
+++ rentalCars/js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin: 100,
+    nav: false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:6
+        },
+        1000:{
+            items:6
+        }
+    }
+})
