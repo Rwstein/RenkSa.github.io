57cd021b73a8bbcfc645def8fa5ffe96643e20b1
Renk Willy da Mota Sá <82468145+RenkSa@users.noreply.github.com>
2022-06-14 15:22:07 -0300

    update

--- rentalCars/js/main.js
+++ rentalCars/js/main.js
@@ -57,32 +57,6 @@ function Whatsapplink(){
 }
 window.addEventListener('scroll', Whatsapplink)
 
-
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'bx-sun'
-
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
-
-if (selectedTheme) {
-
-document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
-}
-
-themeButton.addEventListener('click', () => {
-
-  document.body.classList.toggle(darkTheme)
-  themeButton.classList.toggle(iconTheme)
-
-  localStorage.setItem('selected-theme', getCurrentTheme())
-  localStorage.setItem('selected-icon', getCurrentIcon())
-})
-
 const sr = ScrollReveal({
   origin: 'top',
   distance: '30px',
