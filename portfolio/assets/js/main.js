b024079695f1c779da044d095bc92aa205983aa8
Renk Willy da Mota Sá <82468145+RenkSa@users.noreply.github.com>
2022-06-14 19:53:33 -0300

    revisando

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -90,13 +90,12 @@ const sr = ScrollReveal({
 });
 
 
-
+            
 sr.reveal(`.home__data, .home__img,
             .about__data, .about__img,
-            .services__content, .menu__content,
-            .app__data, .app__img,
+            .services__content, .app__data, .app__img,
             .contact__data, .contact__button,
-            .footer__content`, {
+.footer__content`, {
     interval: 50
 });
 
